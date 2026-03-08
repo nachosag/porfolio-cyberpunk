4c3bb61429d6d7017d951659a97dca0156816177
nachosag <ignacioborlenghi@gmail.com>
2026-03-07 21:23:49 -0300

    Remove all project files and configurations for a complete reset of the Vite React TypeScript starter template.

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="py-8 text-center text-gray-500">
-      <p>© 2024 CYBER_DEV // ALL RIGHTS ENCRYPTED</p>
-    </footer>
-  );
-}
